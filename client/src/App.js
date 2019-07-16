9383acef5bb456ecce0210f5312ad9ac1356530f
SeanJFox <seanjacobfox@gmail.com>
2019-07-16 19:17:12 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import socketIOClient from 'socket.io-client';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  };
+
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    )
+  };
+
+};
+
+export default App;
